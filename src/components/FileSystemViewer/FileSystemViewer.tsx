d46d403ff1d20506fd83d796e1def30e07af3841
Guido Pereira <stereopt@gmail.com>
2024-08-29 17:41:25 +0100

    feat: finished file system

--- src/components/FileSystemViewer/FileSystemViewer.tsx
+++ src/components/FileSystemViewer/FileSystemViewer.tsx
@@ -1,25 +1,38 @@
-import Folder, { FolderType } from './Folder';
+import FileSystemItem, { Node } from './FileSystemItem';
 
-const folders: FolderType[] = [
+const nodes: Node[] = [
   {
     name: 'Home',
-    folders: [
+    nodes: [
       {
         name: 'Movies',
-        folders: [
+        nodes: [
           {
             name: 'Action',
-            folders: [
-              { name: '2000s', folders: [{ name: 'Popular' }] },
-              { name: '2010s' },
+            nodes: [
+              {
+                name: '2000s',
+                nodes: [
+                  { name: 'Gladiator.mp4' },
+                  { name: 'American-Beauty.mp4' },
+                ],
+              },
+              { name: '2010s', nodes: [] },
             ],
           },
-          { name: 'Comedy', folders: [{ name: '2000s' }] },
+          { name: 'Comedy', nodes: [{ name: '2000s', nodes: [] }] },
         ],
       },
-      { name: 'Music', folders: [{ name: 'Rock' }, { name: 'Classical' }] },
-      { name: 'Pictures' },
-      { name: 'Documents' },
+      {
+        name: 'Music',
+        nodes: [
+          { name: 'Rock', nodes: [] },
+          { name: 'Classical', nodes: [] },
+        ],
+      },
+      { name: 'Pictures', nodes: [] },
+      { name: 'Documents', nodes: [] },
+      { name: 'passwords.txt' },
     ],
   },
 ];
@@ -28,8 +41,8 @@ const FileSystemViewer = () => {
   return (
     <div className="p-8 max-w-sm mx-auto">
       <ul>
-        {folders.map((folder) => {
-          return <Folder folder={folder} key={folder.name} />;
+        {nodes.map((node) => {
+          return <FileSystemItem node={node} key={node.name} />;
         })}
       </ul>
     </div>
